3914729b9b92f23b9ee6a974473d5fbcbad5692d
NehaSarahJoseph <nehakottayil23@gmail.com>
2023-04-24 17:52:44 +0530

    Added even or odd program code

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+if num == 0:
+    print("Type a number greater than 0")
+elif num % 2 == 0:
+    print("Its an even number")
+else:
+    print("Its an odd number")
+
+
